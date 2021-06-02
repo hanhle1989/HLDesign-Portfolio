9c17f38d53cc943f4f395163e7d53eaa47c909fc
Hanh Le <hanhdtle@gmail.com>
2021-06-01 20:32:05 -0700

    trying to fix routes

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component= {Home} />
-        <Route exact path="./pages/home" component={Home} />
-        <Route exact path="./pages//webdevelopment" component={Webdevelopment} />
-        <Route exact path="./pages/contact" component={Contact} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/webdevelopment" component={Webdevelopment} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
       <Footer />
     </HashRouter>
